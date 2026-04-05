b785b468e429edb1cea56c648a57189b3e4bac2e
Dishant <dishant.purabiya@dhyey.com>
2026-04-05 08:55:57 +0530

    lecture 4

---
+++ demo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cfloat> // For DBL_MAX
+using namespace std;
+int main() {
+    int Number;
+cout << "enter Number : ";
+cin >> Number;
+int i;
+cout << "enter Number : ";
+i = 1;
+   return 0;
+}
